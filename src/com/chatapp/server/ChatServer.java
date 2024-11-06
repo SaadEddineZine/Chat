7c75177f9a61e422bbdc61e8a2d99b440601bac0
Saad Zine <ikeepmyskills24@gmail.com>
2024-11-06 19:44:09 +0100

    New Line!

--- src/com/chatapp/server/ChatServer.java
+++ src/com/chatapp/server/ChatServer.java
@@ -4,6 +4,7 @@ public class ChatServer {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Hello Again!");
+        System.out.println("New Line!");
     }
     
 }
